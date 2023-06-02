428f286731b410ca8b8f3f0a336b6afe372e7a71
sam89001 <samscreativespace@gmail.com>
2023-06-02 20:07:11 +0100

    main bug page updates

--- routes/mainpage.js
+++ routes/mainpage.js
@@ -5,4 +5,8 @@ router.get('/', (req, res) => {
     res.render('mainscreen/main-screen') //this is where in the folders its looking at the file
 }) 
 
+router.get('/bugscreen', (req, res) => {
+    res.render('mainscreen/bug-page')
+}) 
+
 module.exports = router
